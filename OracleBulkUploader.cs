5166b076c6f8550fbedb7b9f72335596d14d3285
Павел Трушин <ptrushin@custis.ru>
2019-07-23 10:08:10 +0300

    Initial

---
+++ OracleBulkUploader.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Oracle.ManagedDataAccess.Client;
+
+namespace EFCore.OracleBulkUploader
+{
+    /// <summary>OracleBulkUploader</summary>
+    public static class OracleBulkUploader
+    {
+        /// <summary>BulkInsert</summary>
+        public static bool Insert<T>(DbContext dbContext, List<T> list)
+            where T : class
+        {
+            var model = dbContext.Model.GetEntityTypes().Where(e => e.ClrType == typeof(T)).Single();
+            var tableName = model.Relational().TableName;
+            var columnNames = model.GetProperties().Select(e => e.Relational().ColumnName).ToList();
+
+            string query = $"iNSERT INTO {tableName} ({string.Join(",", columnNames.Select(e => $"{e}"))}) VALUES ({string.Join(",", columnNames.Select(e => $":{e}"))})";
+            
+            using (var command = (OracleCommand)dbContext.Database.GetDbConnection().CreateCommand())
+            {
+                command.CommandText = query;
+                command.CommandType = CommandType.Text;
+                command.BindByName = true;
+                command.ArrayBindCount = list.Count;
+
+                foreach (var c in model.GetProperties())
+                {
+                    var item = Expression.Parameter(typeof(T), "item");
+                    var prop = Expression.Convert(Expression.Property(item, c.PropertyInfo.Name), typeof(object));
+                    var f = Expression.Lambda<Func<T, object>>(prop, item).Compile();
+
+                    command.Parameters.Add($":{c.Relational().ColumnName}", GetDbType(c.ClrType), list.Select(e => f(e)).ToArray(), ParameterDirection.Input);
+                }
+
+                int result = command.ExecuteNonQuery();
+                return result == list.Count;
+            }
+        }
+
+        private static OracleDbType GetDbType(Type type)
+        {
+            if (type == typeof(string)) return OracleDbType.Varchar2;
+            if (type == typeof(short) || type == typeof(Nullable<short>)) return OracleDbType.Int16;
+            if (type == typeof(int) || type == typeof(Nullable<int>)) return OracleDbType.Int32;
+            if (type == typeof(long) || type == typeof(Nullable<long>)) return OracleDbType.Int64;
+            if (type == typeof(decimal) || type == typeof(Nullable<decimal>)) return OracleDbType.Decimal;
+            if (type == typeof(DateTime) || type == typeof(Nullable<DateTime>)) return OracleDbType.Date;
+            if (type == typeof(bool) || type == typeof(Nullable<bool>)) return OracleDbType.Int16;
+
+            throw new ArgumentException(type.ToString());
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # EFCore.OracleBulkUploader
+
+BulkInsert in Oracle from list of ercore entities.
+
+```
+public class User
+{
+    [Key]
+    public long Id { get;set; }
+    public string Name { get; set; }
+}
+```
+
+```
+var list = new List<User>();
+...
+OracleBulkUploader.Insert(DbContext, list);
+```
+
+# Links
+* Nuget - https://www.nuget.org/packages/EFCore.OracleBulkUploader/
